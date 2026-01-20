417c3a29382c2a54cf5ab6973eba51ac6cf3e078
Michał Dziwisz <michal@dziwisz.net>
2026-01-20 02:23:53 +0100

    Adopt NVDA add-on template scaffold

---
+++ buildVars.py
@@ -0,0 +1,86 @@
+# Build customizations
+# Change this file instead of sconstruct or manifest files, whenever possible.
+
+from site_scons.site_tools.NVDATool.typings import AddonInfo, BrailleTables, SymbolDictionaries
+
+# Since some strings in `addon_info` are translatable,
+# we need to include them in the .po files.
+# Gettext recognizes only strings given as parameters to the `_` function.
+# To avoid initializing translations in this module we simply import a "fake" `_` function
+# which returns whatever is given to it as an argument.
+from site_scons.site_tools.NVDATool.utils import _
+
+
+# Add-on information variables
+addon_info = AddonInfo(
+	addon_name="apollo",
+	# Translators: Summary/title for this add-on to be shown in the add-on store.
+	addon_summary=_("Dolphin Apollo 2 synthesizer driver"),
+	# Translators: Long description to be shown for this add-on in the add-on store.
+	addon_description=_(
+		"Adds support for Dolphin Apollo 2 / PC 2 Card / Juno serial speech synthesizers."
+	),
+	addon_version="0.1.0",
+	# Translators: What's new content for the add-on version to be shown in the add-on store.
+	addon_changelog=_("Initial public release (work in progress)."),
+	addon_author="PomPa <Kontakt@napompuj.SE>",
+	addon_url="https://github.com/michaldziwisz/apollo",
+	addon_sourceURL="https://github.com/michaldziwisz/apollo",
+	addon_docFileName="readme.html",
+	addon_minimumNVDAVersion="2024.1.0",
+	addon_lastTestedNVDAVersion="2025.3.2",
+	addon_updateChannel=None,
+	addon_license="GPL-2.0-or-later",
+	addon_licenseURL="https://www.gnu.org/licenses/old-licenses/gpl-2.0.html",
+)
+
+# Define the python files that are the sources of your add-on.
+# You can either list every file (using ""/") as a path separator,
+# or use glob expressions.
+# For example to include all files with a ".py" extension from the "globalPlugins" dir of your add-on
+# the list can be written as follows:
+# pythonSources = ["addon/globalPlugins/*.py"]
+# For more information on SCons Glob expressions please take a look at:
+# https://scons.org/doc/production/HTML/scons-user/apd.html
+pythonSources: list[str] = ["addon/synthDrivers/**/*.py"]
+
+# Files that contain strings for translation. Usually your python sources
+i18nSources: list[str] = pythonSources + ["buildVars.py"]
+
+# Files that will be ignored when building the nvda-addon file
+# Paths are relative to the addon directory, not to the root directory of your addon sources.
+# You can either list every file (using ""/") as a path separator,
+# or use glob expressions.
+excludedFiles: list[str] = []
+
+# Base language for the NVDA add-on
+# If your add-on is written in a language other than english, modify this variable.
+# For example, set baseLanguage to "es" if your add-on is primarily written in spanish.
+# You must also edit .gitignore file to specify base language files to be ignored.
+baseLanguage: str = "en"
+
+# Markdown extensions for add-on documentation
+# Most add-ons do not require additional Markdown extensions.
+# If you need to add support for markup such as tables, fill out the below list.
+# Extensions string must be of the form "markdown.extensions.extensionName"
+# e.g. "markdown.extensions.tables" to add tables.
+markdownExtensions: list[str] = []
+
+# Custom braille translation tables
+# If your add-on includes custom braille tables (most will not), fill out this dictionary.
+# Each key is a dictionary named according to braille table file name,
+# with keys inside recording the following attributes:
+# displayName (name of the table shown to users and translatable),
+# contracted (contracted (True) or uncontracted (False) braille code),
+# output (shown in output table list),
+# input (shown in input table list).
+brailleTables: BrailleTables = {}
+
+# Custom speech symbol dictionaries
+# Symbol dictionary files reside in the locale folder, e.g. `locale\en`, and are named `symbols-<name>.dic`.
+# If your add-on includes custom speech symbol dictionaries (most will not), fill out this dictionary.
+# Each key is the name of the dictionary,
+# with keys inside recording the following attributes:
+# displayName (name of the speech dictionary shown to users and translatable),
+# mandatory (True when always enabled, False when not.
+symbolDictionaries: SymbolDictionaries = {}
